16655ce12981e195aa4c016c2d597add9a296e30
kxzyDEV <112576984+kxzyDEV@users.noreply.github.com>
2022-09-04 21:38:05 +0200

    Update NovusHub2.lua

--- NovusHub2.lua
+++ NovusHub2.lua
@@ -4,3 +4,6 @@ end
 if game.PlaceId == 292439477 then -- Phantom Forces
     loadstring(game:HttpGet(('https://raw.githubusercontent.com/kxzyDEV/NovusHub/main/Phantom%20Forces.lua'),true))()   
 end
+f game.PlaceId == 255236425 then -- Traitor Town
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/kxzyDEV/NovusHub/main/Traitor%20Town.lua'),true))()   
+end
