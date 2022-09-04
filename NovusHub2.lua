d09e83b45b585da8a54198c9112e3576d417eaa9
kxzyDEV <112576984+kxzyDEV@users.noreply.github.com>
2022-09-04 16:33:09 +0200

    Add files via upload

---
+++ NovusHub2.lua
@@ -0,0 +1,6 @@
+if game.PlaceId == 286090429 then -- Arsenal
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/kxzyDEV/NovusHub/main/Arsenal.lua'),true))()   
+end
+if game.PlaceId == 292439477 then -- Phantom Forces
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/kxzyDEV/NovusHub/main/Phantom%20Forces.lua'),true))()   
+end
