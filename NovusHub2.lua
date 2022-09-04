39e3ac727e6a0fc13bc84fb600c202d63de2556e
kxzyDEV <112576984+kxzyDEV@users.noreply.github.com>
2022-09-04 22:35:32 +0200

    Update NovusHub2.lua

--- NovusHub2.lua
+++ NovusHub2.lua
@@ -157,3 +157,11 @@ end
 end
 end
 end)
+local __namecall;
+__namecall = hookmetamethod(game, "__namecall", function(self, ...)
+   local method = getnamecallmethod()
+   if method == "FireServer" and self.IsA(self, "RemoteEvent") and self == game.GetService(game, "ReplicatedStorage").ServerEvents.Fall then
+       return
+   end  
+   return __namecall(self, ...)
+end)
